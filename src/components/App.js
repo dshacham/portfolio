1faa58e6364e869d548e9324d04fa24609aec7c6
Dana Shacham <dshacham22@gmail.com>
2020-08-01 19:22:28 +0200

    add resp. design

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Context from './Context';
 import NavBar from './NavBar';
 import Home from './Home';
 import Footer from './Footer';
+import SlideInNav from './SlideInNav';
 
 function App() {
+  const [navClass, setNavClass] = useState('/');
+  const [winWidth, setWinWidth] = useState('');
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, []);
+
+  useEffect(() => {
+    window.innerWidth > 768 ?
+      setWinWidth('desktop')
+      :
+      setWinWidth('mobile')
+  }, []);
 
   return (
     <div className="App">
-      <Context.Provider>
+      <Context.Provider value={{ navClass, setNavClass, winWidth, setWinWidth }}>
         <Router>
-          <NavBar />
+          {winWidth === 'desktop' ?
+            <NavBar />
+            :
+            <SlideInNav />
+          }
           <Home />
           <Footer />
         </Router>
